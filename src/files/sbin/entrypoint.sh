dce6d07257cae77f06c4d11d347d750b43bb3d54
Adam Russell <241628+a1russell@users.noreply.github.com>
2025-04-18 14:00:19 -0500

    Make entrypoint script safer.

--- src/files/sbin/entrypoint.sh
+++ src/files/sbin/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-for f in /sbin/entrypoint.d/*.sh; do
-  /bin/sh "$f"
-done
+if [ "$(ls /sbin/entrypoint.d/*.sh 2> /dev/null | wc --lines)" -gt "0" ]; then
+  for f in /sbin/entrypoint.d/*.sh; do
+    /bin/sh "$f"
+  done
+fi
 exec "$@"
